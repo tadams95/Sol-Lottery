2243eb650a449831cd842863b5ff3774e1cc1544
tadams95 <tyrelleadams28@gmail.com>
2023-02-24 16:23:24 -0800

    Render Contract Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import web3 from './web3';
+import React from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
 
-function App() {
-  
-  return (
-    
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: "",
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Lottery Dapp</h1>
+        <p>This contract is managed by:{this.state.manager}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
---
+++ client/src/lottery.js
@@ -0,0 +1,81 @@
+import web3 from "./web3";
+
+const address = "0x76588Dc9c0F58486FC8Fa38384848B4ff05cFd0B";
+
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getPlayers",
+    outputs: [
+      {
+        internalType: "address payable[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "lottery",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "players",
+    outputs: [
+      {
+        internalType: "address payable",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const lottery = new web3.eth.Contract(abi, address);
+
+export default lottery;
